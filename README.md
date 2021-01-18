afaf2aea3feb8d142d2ebc8de4d773225801671e
Jungin <jungin3486@gmail.com>
2021-01-18 23:43:57 +0900

    second

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # Covid_data_crawling
+
 covid_data_crawling by Python
 
 https://www.worldometers.info/coronavirus/
+
+- soup 객체에 있는 국가들의 코로나 관련 데이터에 대해 크롤링
+- 크롤링된 코로나 관련 데이터 중 국가 이름이 key, 확진자, 사망자, 완치가 value인 딕셔너리 형태로 저장
+- 각 데이터는 정수 자료형으로 표현하되 유효하지 않은 값은 문자열 "N/A"로 처리
--- crawler_v2.py
+++ crawler_v2.py
@@ -1,54 +1,59 @@
 import requests
 import re
 from bs4 import BeautifulSoup
+import numpy as np
 
 def crawling(soup):
-    # country_all = [] 
-    # country_list = soup.find("tbody").find_all("a",class_="mt_a")
-    # for country in country_list:
-    #     country_all.append(country.get_text())
-    # country_all.insert(0,"World")
-    # #print(country_all)
-
-
-
-    result_old = []
-    result_new = []
     result_list = soup.find("tbody").find_all("tr")
-    
+    covid = []
     for res in result_list:
-        #res_old = res.get_text()
-        res_old = res.get_text().split()
-        #res_new = re.findall("\d+",res_old)
-
-        result_old.append(res_old)
-        #result_new.append(res_new)
-    #result = result[7:]
-    print(res_old)
-
-    # for i in range(1,len(result)):
-    #     del result[i][0]
-        
-        
-
-    # result2 = []
-    # for i in range(len(result)):
-    #     del result[i][6:]
-    #     del result[i][2] 
-    #     del result[i][3]
-        
-    # #print(result)
+        cnt = 0
+        for r in res.find_all("td"):
+            if cnt==1 or cnt ==2 or cnt==4 or cnt==6:
+                temp = r.get_text().replace(",","").replace("\n","").replace("\t","")
+                if cnt == 1:
+                    covid.append(temp)
+                else:
+                    if temp == 'N/A':
+                        covid.append(temp)
+                    elif temp == ' ' or temp=='':
+                        covid.append('N/A')
+                    else:
+                        covid.append(int(temp)) 
+            cnt +=1
+            
+    #World 부터
+    covid = covid[28:]   
     
- 
-    # data = dict()
-    # for i in range(len(result)):
-    #     data[country_all[i]] = result[i][1:]
-    # print(data)
+    
+    #차원변경
+    covid2 = []
+    temp = []
+    cnt = 1
+    for h in covid:            
+        if cnt !=4:
+            temp.append(h)
+        else:
+            temp.append(h)
+            covid2.append(temp) 
+            temp = [] 
+            cnt = 0
+        cnt+=1
 
+
+    
+    data = dict()
+    #print(np.shape(covid2)) => (222,4)
+    for i in range(222):
+        data[covid2[i][0]] = {"확진자":covid2[i][1],"사망자":covid2[i][2],"완치":covid2[i][3]}
+        
+    return data
+    
+    
 def main() :
     html = requests.get("https://www.worldometers.info/coronavirus/")
     soup = BeautifulSoup(html.text, "html.parser")
-    crawling(soup)
+    print(crawling(soup))
 
 if __name__ == "__main__" :
     main()
